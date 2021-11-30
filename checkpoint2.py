89a22d610884c9d63991bf3abb39b100e813f28f
mmurad05 <63133020+mmurad05@users.noreply.github.com>
2021-11-29 22:10:17 -0500

    Update checkpoint2.py

--- checkpoint2.py
+++ checkpoint2.py
@@ -19,31 +19,53 @@ import gamebox
 import random
 
 camera = gamebox.Camera(800, 600)
-mouse_sheet = gamebox.load_sprite_sheet("mouse_sprite1.png", 4, 1)
-cat_sheet = gamebox.load_sprite_sheet("cat_sheet.png", 4, 1)
-cat = gamebox.from_image(100,100, cat_sheet[0])
-mouse = gamebox.from_image(400, 300, mouse_sheet[0])
-cheese = gamebox.from_image(200,200, "cheese.png")
+mouse_sheet = None
+cat_sheet = None
+cat = None
+mouse = None
+cheese = None
+game_over = None
+start_page = None
 score = 0
 alive = True
+started = False
+start_page = gamebox.from_image(400,300, "Start Page.jpg")
+def setup():
+    global mouse_sheet, cat_sheet, cat, mouse, cheese, game_over, score, alive
+    mouse_sheet = gamebox.load_sprite_sheet("mouse_sprite1.png", 4, 1)
+    cat_sheet = gamebox.load_sprite_sheet("cat_sheet.png", 4, 1)
+    cat = gamebox.from_image(100, 100, cat_sheet[0])
+    mouse = gamebox.from_image(400, 300, mouse_sheet[0])
+    cheese = gamebox.from_image(200, 200, "cheese.png")
+    game_over = gamebox.from_text(400, 300, "Game Over", 72, "red")
+    score = 0
+    alive = True
+setup()
+
 def tick(keys):
-    global alive, score
-    if alive:
-        mouse_movement(keys)
-        cat_movement(keys)
-        if mouse.touches(cheese):
-            cheese.x = random.randint(20, 780)
-            cheese.y = random.randint(20, 580)
-            score += 1
-    score_display = gamebox.from_text(40, camera.y + 250, str(score), 50, "red")
+    global alive, score, started, start_page
     camera.clear("white")
+    score_display = gamebox.from_text(40, camera.y + 250, str(score), 50, "red")
     camera.draw(cheese)
     camera.draw(score_display)
     camera.draw(cat)
     camera.draw(mouse)
+    if started != True:
+        camera.draw(start_page)
+        if pygame.K_r in keys:
+            started = True
+    elif alive:
+        mouse_movement(keys)
+        cat_movement(keys)
+    else:
+        camera.draw(game_over)
+        if pygame.K_SPACE in keys:
+            alive = True
+            setup()
     camera.display()
 
 def mouse_movement(keys):
+    global score
     if pygame.K_UP in keys:
         mouse.y -= 10
         mouse.image = mouse_sheet[3]
@@ -64,8 +86,12 @@ def mouse_movement(keys):
         mouse.image = mouse_sheet[2]
         if mouse.x > 790:
             mouse.x = 790
-
+    if mouse.touches(cheese):
+        cheese.x = random.randint(20, 780)
+        cheese.y = random.randint(20, 580)
+        score += 1
 def cat_movement(keys):
+    global alive
     if pygame.K_w in keys:
         cat.y -= 10
         cat.image = cat_sheet[3]
@@ -86,5 +112,7 @@ def cat_movement(keys):
         cat.image = cat_sheet[2]
         if cat.x > 790:
             cat.x = 790
+    if cat.touches(mouse):
+        alive = False
 ticks_per_second = 30
 gamebox.timer_loop(ticks_per_second, tick)
